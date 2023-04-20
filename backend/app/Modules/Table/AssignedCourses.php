5726fc804528be421a97e6106dac00f94a03067b
gra0298 <gra029801@gmail.com>
2023-04-20 11:56:00 -0500

    featat:Assigned Courses

--- backend/app/Modules/Table/AssignedCourses.php
+++ backend/app/Modules/Table/AssignedCourses.php
@@ -16,19 +16,20 @@ class AssignedCourses{
 
         $school = SchoolQueries::getSchool($request->input('schoolId') ?? 0);
         $db     = "{$school->database_name}";
+        // $school    = SchoolQueries::getSchoolRequest($request);
+        // $db        = $school->db;
+
         $limit  = $request->input('limit') ?? 15;
-        $id = $request->input('pdbPolingStationId');
-
-        $query = DB::table("{$db}.tp_degrees_per_table as tp")
-            ->leftJoin('tp_polling_stations as tm', 'tp.polling_station_id', '=', 'tm.id')
-            ->leftJoin('grados as tg', 'tp.grade_id', '=', 'tg.id')
-            ->select('tp.*', 'tm.table_name', DB::raw('RTRIM(tg.grado) as grado'))
-            ->where('tp.polling_station_id', $id);
+        // $id = $request->input('pdbPolingStationId');
+        $id = $request->input('pdbPolling_stattion_id');
 
 
-
-
-        // return response()->json(['mmsj' => "Hola mundo"]);
+        $query = DB::table("{$db}.tp_degrees_per_table AS tp")
+            ->leftJoin("$db.tp_polling_stations as tm", "tp.polling_station_id", "=", "tm.id")
+            ->leftJoin("$db.grados as tg", "tp.grade_id", "=", "tg.id")
+            ->select("tp.*", "tm.table_name", DB::raw("RTRIM(tg.grado) as grado"))
+            ->where("tp.polling_station_id", $id);
+        // return response()->json(['mmsj' => $id]);
         return self::getResponse(['records' => $query->paginate($limit),'success' => true]);
 
 

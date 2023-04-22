a6a806ddcaf05a6cc1193fda83d830e1374bf01e
Lewis Lopez <lewis.lopez@bit-coco.co>
2023-04-21 23:32:37 -0500

    fix(backend): Fix error report groups directors

--- backend/app/Reports/GroupDirectors.php
+++ backend/app/Reports/GroupDirectors.php
@@ -5,29 +5,18 @@ namespace App\Reports;
 use App\Contracts\ReportProcessorContract;
 use App\Core\JReportModel;
 use Illuminate\Http\Request;
-// Call sp_select_dir_grupo(2018);
 use App\Modules\School\SchoolQueries;
 use App\Traits\MessagesTrait;
 
 class GroupDirectors implements  ReportProcessorContract
 {
-
     use MessagesTrait;
     public function getReport(Request $request): \Illuminate\Http\JsonResponse
     {
-
         try {
-
             $school = SchoolQueries::getSchoolRequest($request);
             $format         = $school->format;
-            $db	            = $school->db;
             $year	        = $school->year;
-            $pdbGrado	    = $request->input('pdbGrado')	;
-            $id_sede	    = $request->input('pdbSede')	;
-            $pdbAll		    = $request->input('pdbAll');
-            $typeReport	    = $request->input('pdbType');
-            // $pdbGrupo		= $request->input('pdbGrupo');
-            // $pdbJorn		= $request->input('pdbJorn');
 
             $report	        = 'dir_grupo';
             $report_export	= 'Lista directores de grupo';

f1822ca7f031fa8b98b3f268558529ed1133e8c0
gra0298 <gra029801@gmail.com>
2023-04-25 17:55:15 -0500

    refactor:.ignore

--- backend/app/Reports/RepresentativeReports.php
+++ backend/app/Reports/RepresentativeReports.php
@@ -20,14 +20,13 @@ class RepresentativeReports implements  ReportProcessorContract
             $format         = $school->format;
             $grade_id	= $request->input('pdbGrado');
 		    $headquarter_id	= $request->input('pdbSede');
-            // $report	        = $request->input('pdbReport');
             $year	        = $school->year;
-            $report_export	= "certificado electoral";
-			$report			=	'certificado_electoral';
+            // $type	        = $request->input('pdbReport');
+            $report			='certificado_electoral';
+            $report_export	= 'certificado electoral';
             $query	=null;
-
             $params         = [
-                // 'P_DB'   =>$db,
+
                 'P_YEAR'    => $year,
                 'P_GRADE_ID' => $grade_id,
                 'P_HEADQUARTER_ID'=>$headquarter_id
@@ -41,13 +40,3 @@ class RepresentativeReports implements  ReportProcessorContract
         }
     }
 }
-//CALL sp_certificado_ficha_informes(  $P{P_DB} $P{P_YEAR} $P{P_GRADE_ID}  $P{P_HEADQUARTER_ID} )
- // "SELECT tm.*, CONCAT(RTRIM(ti.apellido1),' ',RTRIM(ti.apellido2),' ',
-			// 	RTRIM(ti.nombre1),' ',RTRIM(ti.nombre2)) AS nombres,
-			// 	RTRIM(tg.grado) AS grado, RTRIM(ts.headquarters_name) AS sede, ti.foto, ti.mime
-			// 	FROM {$db}.student_enrollment AS tm
-			// 	LEFT JOIN {$db}.inscripciones AS ti ON tm.id_student = ti.id
-			// 	LEFT JOIN {$db}.grados AS tg ON tm.id_grade = tg.id
-			// 	LEFT JOIN {$db}.sedes AS ts ON tm.id_headquarters = ts.id
-			// 	WHERE tm.year	= {$year} AND tm.id_grade = {$grade_id} AND tm.id_headquarters = {$headquarter_id}
-			// 	ORDER BY tm.id_grade ,tm.id_group,nombres";

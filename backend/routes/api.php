01523e315c72cd1085d8ceeea6b77e913dfff20f
Lewis Lopez <lewis.lopez@bit-coco.co>
2023-05-01 08:02:36 -0500

    refactor: bug sonar

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::prefix('v1')->group(function () {
-    require __DIR__.'/authentication.php';
+    require_once __DIR__.'/authentication.php';
     Route::group(['middleware' => 'auth:api'], function () {
         Route::apiResource('crud', 'SchoolMasterController');
         Route::resource('/index',       'MasterController');
@@ -19,21 +19,21 @@ Route::prefix('v1')->group(function () {
             Route::get('cities',                'getCities');
             Route::get('countries',             'getCountries');
         });
-        require __DIR__.'/school.php';
-        require __DIR__.'/academic.php';
-        require __DIR__.'/courses.php';
+        require_once __DIR__.'/school.php';
+        require_once __DIR__.'/academic.php';
+        require_once __DIR__.'/courses.php';
         Route::prefix('teachers')->group(function () {
             Route::controller('Administrative\TeachersController')->group(function () {
                Route::get('get-by-year', 'getByYear');
             });
         });
-        require __DIR__.'/students.php';
-        require __DIR__.'/representative.php';
-        require __DIR__.'/files.php';
-        require __DIR__.'/exports.php';
-        require __DIR__.'/reports.php';
-        require __DIR__.'/settings.php';
-        require __DIR__.'/academic-leveling.php';
-        require __DIR__.'/promotion.php';
+        require_once __DIR__.'/students.php';
+        require_once __DIR__.'/representative.php';
+        require_once __DIR__.'/files.php';
+        require_once __DIR__.'/exports.php';
+        require_once __DIR__.'/reports.php';
+        require_once __DIR__.'/settings.php';
+        require_once __DIR__.'/academic-leveling.php';
+        require_once __DIR__.'/promotion.php';
     });
 });

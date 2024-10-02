90bee078f60620d472657e15f46eebc630752a50
Lewis Lopez <lopezsoft.com@gmail.com>
2024-10-01 22:54:07 -0500

    Migracion v15 to v16

--- fronts/students/src/app/auth/helpers/auth.guards.ts
+++ fronts/students/src/app/auth/helpers/auth.guards.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { AuthenticationService } from 'app/services/users';
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   /**
    *
    * @param {Router} _router
